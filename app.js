225d925e3cc61c511cc887fb9f0e6364cf59af3c
nicola-20 <35260168+nicola-20@users.noreply.github.com>
2018-11-08 14:54:09 +0000

    Made a few changes

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const apiRouter = require('./routes/api');
 const { DB_URL } = require('./config')
-// const { handle400s, handle404s, handle500s } = require('./errors')
 
 mongoose.connect(DB_URL, { useNewUrlParser: true })
   .then(() => {
@@ -17,10 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
   
 app.use(express.static('public'));
-  
-// app.get('/', (req, res, next) => {
-//   res.render('index')
-// })
+
 
 app.use('/api', apiRouter);
   
@@ -28,19 +24,16 @@ app.use('/*', (req, res, next) => {
   next({ status: 404, msg: 'Path not found' })
 })
   
-// app.use(handle400s);
-// app.use(handle404s);
-// app.use(handle500s);
+
 
 app.use((err, req, res, next) => {
 // console.log(err)
   if (err.status) { // if err has a status
 
-    if (err.msg || err.message) { // if err has a message
+    if (err.msg || err.message) {
       res.status(err.status).send(err)
-
-    } else { // if err has status but no message
-      err.msg = "Error"
+    } else {
+      err.msg = "Unknown error"
       res.status(err.status).send(err)
     }
 
@@ -54,17 +47,11 @@ app.use((err, req, res, next) => {
       err.msg = err.message
     } else {
       err.status = 500
+      err.msg = 'Internal Server Error'
     } 
 
     res.status(err.status).send(err)
   }
-    
-    // 400 Bad Request
-    // 404 Not Found
-    // 405 Method Not Allowed
-    // 500 Internal Server Error
-  
-    // res.status(500).send({ msg: 'Internal Server Error' })
 })
 
 module.exports = app;

38707a04cb471d43364118d83c77313929b75a96
nicola-20 <35260168+nicola-20@users.noreply.github.com>
2018-11-27 10:19:48 +0000

    Reseeded database

--- seed/seed.dev.js
+++ seed/seed.dev.js
@@ -1,8 +1,8 @@
 process.env.NODE_ENV = 'production'
 const seedDB = require('./seed.js');
 const mongoose = require('mongoose');
-const { DB_URL, datapath } = process.env.NODE_ENV === 'production' ? process.env : require('../config');
-const rawData = require(`../seed/${datapath}`)
+const { DB_URL, datapath } = require('../config');
+const rawData = require(`../seed/${datapath || 'devData'}`)
 
 mongoose
   .connect(DB_URL, { useNewUrlParser: true })
